6ab67375d3d588ece8c84d076010fe64ee946a7e
佐藤優衣 <yui.chocolat1538@gmail.com>
2017-05-23 19:49:46 +0900

    新しいVCを追加

--- AppTimer/ViewController.swift
+++ AppTimer/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count: Double = 60.00
+    var timer = Timer()
+    
+    @IBOutlet var timeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.up), userInfo: nil, repeats: true)
+            
+        }
+        
+        up()
+    
+    }
+    
+    func up() {
+        count = count - 0.1
+        timeLabel.text = String(format:"%d" , count)
     }
 
     override func didReceiveMemoryWarning() {

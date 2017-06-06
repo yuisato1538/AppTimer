6173bcb2f21f8908c3117dbd608e8742c3e3b546
佐藤優衣 <yui.chocolat1538@gmail.com>
2017-06-06 19:54:39 +0900

    ２つのピッカーにtagをつけて行数とか列数のところをif文で分けたけどcountのところが色々わからない

--- AppTimer/ViewController.swift
+++ AppTimer/ViewController.swift
@@ -11,28 +11,12 @@ import UIKit
 class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
     
     @IBOutlet var timePickerView: UIPickerView!
+    @IBOutlet var appPickerView: UIPickerView!
     
     
     var selected = "3"
     
-    let dataSelect = ["5","10","15"]
-    //列数を返す
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    //行数を返す
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return dataSelect.count
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return dataSelect[row]
-    }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        //        timelabel.text="\(dataSelect[row])"
-        
-        selected = dataSelect[row]
-        print(selected)
-    }
+    let dataSelect = ["5","10","15","20","25","30"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +24,11 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         
         timePickerView.delegate = self
         timePickerView.dataSource = self
+        timePickerView.tag = 1
+        
+        appPickerView.delegate = self
+        appPickerView.dataSource = self
+        appPickerView.tag = 2
     }
     
     override func didReceiveMemoryWarning() {
@@ -62,6 +51,36 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         self.performSegue(withIdentifier: "toTimeVC", sender:nil)
     }
     
+    
+    //列数を返す
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        if pickerView.tag == 1 {
+            return 1
+        } else if pickerView.tag == 2 {
+            return 1
+        }
+        return 0
+    }
+    //行数を返す
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 {
+            return dataSelect.count
+        } else if pickerView.tag == 2 {
+            return dataSelect.count
+        }
+        return dataSelect.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSelect[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //        timelabel.text="\(dataSelect[row])"
+        
+        selected = dataSelect[row]
+        print(selected)
+    }
+    
+
     
 }
 
